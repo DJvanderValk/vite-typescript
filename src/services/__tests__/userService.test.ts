b6e0cf4aaa41e8cf54dd7ca0e55a99b2a19b2977
Dennis van der Valk <dennisvdvalk@hotmail.com>
2023-12-03 23:18:02 +0100

    Use vitest

--- src/services/__tests__/userService.test.ts
+++ src/services/__tests__/userService.test.ts
@@ -1,9 +1,13 @@
-import { describe, expect, it } from '@jest/globals';
+import { expect, describe, it } from 'vitest';
 
 // import userService from '../userService';
 
 describe('test', () => {
 	it('1', () => {
 		expect(1).toBe(1);
+	});
+	
+	it('2', () => {
+		expect(1).toBe(2);
 	});
 });
--- src/utils/jest/jest.envSetup.ts
+++
@@ -1,8 +0,0 @@
-import * as path from 'path';
-import { jest } from '@jest/globals';
-
-// https://jestjs.io/docs/ecmascript-modules
-// dotenv.config({
-// 	path: path.resolve(process.cwd(), '.env'),
-// });
-console.log(import.meta.jest);
